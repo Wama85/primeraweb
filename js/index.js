1941be91f2a98e608f7cc8faf646f77233c1ef7b
susan-cespedes <wi_an9@hotmail.com>
2024-02-27 18:03:04 -0400

    javascript datos

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const nombre = document.getElementById("txtnombre");
+const mostrar = document.getElementById("uno");
+
+nombre.addEventListener("input", mostrarNombre);
+
+function mostrarNombre() {
+    if (nombre.value ==""){
+        mostrar.innerHTML = "LOREM IPSUM";
+    }
+    else
+    mostrar.innerHTML = `
+<p style="color:red">${nombre.value}</p>`;
+}
